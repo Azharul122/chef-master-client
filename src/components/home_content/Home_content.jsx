1b4259d26800bc899738f23afcfe218936a12bb7
Azharul Islam <azharulip@gmail.com>
2023-05-01 22:46:02 +0600

    routing and navbar done

---
+++ src/components/home_content/Home_content.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home_content = () => {
+    return (
+        <div>
+            <h2>Home Component</h2>
+        </div>
+    );
+};
+
+export default Home_content;
---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Common from './components/Common/Common.jsx';
+import Home_content from './components/home_content/Home_content.jsx';
+import Blog from './components/Blog/Blog.jsx';
+import About from './components/About/About.jsx';
+import Login from './components/login/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Common></Common>,
+    children:[
+      {
+        path:"/",
+        element:<Home_content></Home_content>
+      },
+      {
+        path:"/about",
+        element:<About></About>
+      },
+      {
+        path:"/blog",
+        element:<Blog></Blog>
+      }
+    ]
+  },
+  {
+    path: "login",
+    element: <Login></Login>
+  }
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
